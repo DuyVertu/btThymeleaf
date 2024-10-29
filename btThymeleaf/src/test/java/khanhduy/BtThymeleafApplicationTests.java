fb144b69e99f97a3a8c1d854b931189e24268ac3
Duy_Vertu <112624449+DuyVertu@users.noreply.github.com>
2024-10-29 15:04:14 +0700

    btThymeleaf

---
+++ btThymeleaf/src/test/java/khanhduy/BtThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package khanhduy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
